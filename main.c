04342d2ee6180b8136c495fa2077932cffb486ca
minim0no <ahmed.idrees3110@gmail.com>
2024-02-14 13:42:59 -0600

    fork and exec

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <sys/types.h> 
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
@@ -98,4 +99,26 @@ char **lsh_split_line(char *line) {
     tokens[position] = NULL;
 
     return tokens;
+}
+
+int lsh_launch(char **args) {
+    pid_t pid, wpid;
+    int status;
+
+
+    pid = fork();
+    if (pid < 0) {
+        perror("lsh");
+    } else if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    } else {
+        do {
+        wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
 }
