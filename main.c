c5d540ebfc5deddfc04f264a74912c9da304feae
minim0no <ahmed.idrees3110@gmail.com>
2024-02-14 14:08:57 -0600

    Add built-ins and execution function

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <sys/types.h> 
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
@@ -121,4 +125,76 @@ int lsh_launch(char **args) {
     }
 
     return 1;
+}
+
+/*
+    Function Declarations for shell commands:
+*/
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+/*
+    List for commands and their functions:
+*/
+char *builtin_str[] = {
+    "cd", 
+    "help", 
+    "exit"
+};
+
+int (*builtin_func[]) (char**) = {
+    &lsh_cd, 
+    &lsh_help, 
+    &lsh_exit
+};
+
+int lsh_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+/*
+    function implementations:
+*/
+int lsh_cd(char **args) {
+    if (args[1] == NULL) {
+        fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("lsh");
+        }
+    }
+
+    return 1;
+}
+
+int lsh_help(char **args) {
+    printf("LSH :D\n");
+    printf("Type program names and argument, and hit enter\n");
+    printf("The following are build in commands: \n");
+
+    for (int i = 0; i < lsh_num_builtins(); i++) {
+        printf("    %s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for information on other programs. \n");
+    return 1;
+}
+
+int lsh_exit(char **args) {
+    return 0;
+}
+
+int lsh_execute(char **args) {
+    if (args[0] == NULL) {
+        return 1;
+    }
+
+    for (int i = 0; i < lsh_num_builtins(); i++) {
+        if (strcmp(args[0], builtin_str[i]) == 0) {
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    return lsh_launch(args);
 }
